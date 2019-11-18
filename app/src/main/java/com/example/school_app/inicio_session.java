2197da9f2308474eba068aebb3147641bc2339a1
JHPAT <sanhool@live.com.mx>
2019-11-18 14:45:29 -0500

    login_diseño

--- app/src/main/java/com/example/school_app/inicio_session.java
+++ app/src/main/java/com/example/school_app/inicio_session.java
@@ -2,7 +2,10 @@ package com.example.school_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class inicio_session extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class inicio_session extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_session);
+        Button b_1;
+        b_1=findViewById(R.id. btn_login);
+        b_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent x = new Intent (inicio_session.this, MainActivity.class);
+                startActivity(x);
+            }
+        });
     }
+
 }
